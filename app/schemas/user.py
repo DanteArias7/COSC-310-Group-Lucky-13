42d496e644ba5042dbd775a76c36ad4f990a2f10
DanteArias7 <Danteari7@gmail.com>
2026-02-19 12:53:20 -0800

    User Model with fixed pylint warnings

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,8 +1,9 @@
+"""Schemas for User Model"""
 from sqlmodel import SQLModel, Field
 from pydantic import BaseModel
 
-
 class User(SQLModel, table = True):
+    """Schema for a user"""
     id: str = Field(primary_key = True)
     name: str
     email: str
@@ -12,9 +13,11 @@ class User(SQLModel, table = True):
     role: str
 
 class UserCreate(BaseModel):
+    """Schema for creating a user"""
     name: str
     email: str
     phone_number: str
     address: str
     password: str
-    role: str 
+    role: str
+    
