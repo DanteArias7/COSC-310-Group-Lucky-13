3c67f9e51f4985b9be655ae1fe13b37813947738
DanteArias7 <Danteari7@gmail.com>
2026-02-22 11:41:18 -0800

    Refactored router to use Dependecny injection for repo

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,14 +1,20 @@
 """User API endpoints"""
-from fastapi import APIRouter
+from pathlib import Path
+
+from fastapi import APIRouter, Depends
 from app.schemas.user import User, UserCreate
 from app.services.user_services import UserServices
 from app.repositories.user_repo import UserRepo
 
 user_router = APIRouter(prefix="/users", tags=["user"])
+USER_DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "users.json"
+
+def create_user_repo():
+    """"Initialize repo object with data path to user json file"""
+    return UserRepo(USER_DATA_PATH)
 
 @user_router.post("", response_model=User, status_code=201)
-def add_user(payload: UserCreate):
+def add_user(payload: UserCreate, repo: UserRepo = Depends(create_user_repo)):
     """API endpoint to create a user"""
-    repo = UserRepo()
     user_service = UserServices(repo)
     return user_service.create_user(payload)
