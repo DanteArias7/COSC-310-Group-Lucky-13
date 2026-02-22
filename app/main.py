6412ca6aee62cf783141d0b94e1cd2243ca401cd
Varun Garg <varunokanagan@gmail.com>
2026-02-21 18:43:36 -0800

    Main file updated to include restaurant router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 """Main application"""
 from fastapi import FastAPI
 from app.routers.user import user_router
-
+from app.routers.restaurant import router
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(router)
