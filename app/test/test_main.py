01b15a51fa7ad6afff07ef817088e818b59790c5
DanteArias7 <113080757+DanteArias7@users.noreply.github.com>
2026-02-22 11:45:57 -0800

    Fix indentation and formatting in test_main.py

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -13,13 +13,13 @@ def test_create_user(tmp_path):
 
     def oveerride_create_user_repo():
         return UserRepo(test_user_data_path)
-    
+
     app.dependency_overrides[create_user_repo] = oveerride_create_user_repo
 
     payload ={"name" : "Alex", "email" : "alexsmith@gmail.com",
                 "phone_number" : "123-456-7890", "address" : "123 Baron Rd, Kelowna, BC, A1B2C3",
                 "password" : "password",  "role" : "customer"}
-    
+
     r = client.post("/users", json=payload)
 
     data = r.json()
