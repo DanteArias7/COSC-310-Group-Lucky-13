b34c4dc579fb4dde4d4918fa657f4a34d71bfe2c
DanteArias7 <Danteari7@gmail.com>
2026-02-24 15:43:55 -0800

    Restaurant routers refactored for DI

--- app/routers/restaurant.py
+++ app/routers/restaurant.py
@@ -1,26 +1,30 @@
 """Restaurant API endpoints"""
 
+from pathlib import Path
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.restaurant import Restaurant
-from app.services.restaurant_services import (
-    fetch_all_restaurants,
-    fetch_restaurant,
-)
+from app.services.restaurant_services import RestaurantServices
+from app.repositories.restaurant_repo import RestaurantRepo
 
+RESTAURANT_DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "restaurants.json"
 restaurant_router = APIRouter(
     prefix="/restaurants",
     tags=["restaurant"],
 )
 
+def create_restaurant_repo():
+    """"Initialize repo object with data path to restaurant json file"""
+    return RestaurantRepo(RESTAURANT_DATA_PATH)
 
 @restaurant_router.get("", response_model=List[Restaurant], status_code=200)
-def get_all_restaurants():
+def get_all_restaurants(repo: RestaurantRepo = Depends(create_restaurant_repo)):
     """Return a list of all restaurants."""
-    return fetch_all_restaurants()
-
+    user_service = RestaurantServices(repo)
+    return user_service.fetch_all_restaurants()
 
 @restaurant_router.get("/{restaurant_id}", response_model=Restaurant, status_code=200)
-def get_restaurant_by_id(restaurant_id: str):
+def get_restaurant_by_id(restaurant_id: str, repo: RestaurantRepo=Depends(create_restaurant_repo)):
     """Return a specific restaurant by its ID."""
-    return fetch_restaurant(restaurant_id)
+    user_service = RestaurantServices(repo)
+    return user_service.fetch_restaurant(restaurant_id)
