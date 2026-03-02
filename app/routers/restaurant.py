0e829ef4c0c9675e8e109a294bc78d98045314dd
zxue-01 <114432708+zxue-01@users.noreply.github.com>
2026-03-02 14:14:03 -0800

    fixing pylint

--- app/routers/restaurant.py
+++ app/routers/restaurant.py
@@ -26,7 +26,7 @@ def create_cart_repo():
     return CartRepo(CART_DATA_PATH)
 
 @restaurant_router.post("", response_model=Restaurant, status_code=201)
-def create_restaurant(payload: RestaurantCreate, 
+def create_restaurant(payload: RestaurantCreate,
                       repo: RestaurantRepo = Depends(create_restaurant_repo)):
     """Create a new restaurant profile."""
     restaurant_service = RestaurantServices(repo)
