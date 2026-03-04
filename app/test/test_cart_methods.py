7c9ee68b06d822ede249b12b4f15104b9ae5f3d0
Varun Garg <gargvarun2000@gmail.com>
2026-03-04 13:22:47 -0800

    Updated unit tests to reflect the changes

--- app/test/test_cart_methods.py
+++ app/test/test_cart_methods.py
@@ -16,7 +16,7 @@ def test_carts():
                             "description": "Plant-based patty with lettuce and tomato",
                             "price": 12.99,
                             "tags": ["vegan"]}],
-                "total" : 7.88}]
+                "total" : 12.99}]
 
 @pytest.fixture
 def mocked_repo(mocker):
@@ -38,6 +38,17 @@ def menu_item_payload():
         description="Crispy golden fries",
         price=5.00,
         tags=["fries"]
+    )
+
+@pytest.fixture
+def menu_item_payload_second():
+    """Second sample menu item payload for cart tests"""
+    return MenuItem(
+        id="018f8c10-7b2a-7f21-9a3c-0a1b2c3d4e03",
+        name="Pizza",
+        description="Cheesy pepperoni pizza",
+        price=15.00,
+        tags=["pizza"]
     )
 
 #delete_menu_item_from_cart Tests
@@ -68,26 +79,63 @@ def test_delete_nonexistent_menu_item_from_cart(test_carts, mocked_repo, mocked_
 
 # add_menu_item_to_cart Tests
 
-def test_add_menu_item_to_nonexistent_cart(test_carts, mocked_repo, mocked_cart_service):
+def test_add_menu_item_to_nonexistent_cart(test_carts, mocked_repo,
+                                           mocked_cart_service, menu_item_payload):
     """Test that add_item_to_cart raises exception if cart not found"""
 
     mocked_repo.load_all_carts.return_value = test_carts
 
     with pytest.raises(HTTPException) as exc_info:
-        mocked_cart_service.add_item_to_cart("fake-id", menu_item_payload)
+        mocked_cart_service.add_item_to_cart("fake-id", test_carts[0]["restaurant_id"],
+                                              menu_item_payload)
 
     assert exc_info.value.status_code == 404
     assert exc_info.value.detail == "Cart fake-id Not Found"
 
-def test_add_menu_item_to_cart(test_carts, mocked_repo, mocked_cart_service, menu_item_payload):
+def test_add_menu_item_to_cart(test_carts, mocked_repo,
+                               mocked_cart_service, menu_item_payload):
     """Test that add_item_to_cart successfully adds an item"""
 
     mocked_repo.load_all_carts.return_value = test_carts
 
     result = mocked_cart_service.add_item_to_cart(
          "00000000-0000-0000-0000-000000000001",
+         test_carts[0]["restaurant_id"],
          menu_item_payload
          )
     assert len(result.menu_items) == 2
     assert result.menu_items[1].id == menu_item_payload.id
+    mocked_repo.save_all_carts.assert_called_once()
+
+# Test that adding an item from a different restaurant raises an exception
+def test_add_menu_item_from_different_restaurant(test_carts, mocked_repo,
+                                                 mocked_cart_service , menu_item_payload_second):
+    """Test that add_item_to_cart raises exception if menu item is from different restaurant"""
+
+    mocked_repo.load_all_carts.return_value = test_carts
+
+    with pytest.raises(HTTPException) as exc_info:
+        mocked_cart_service.add_item_to_cart(
+            "00000000-0000-0000-0000-000000000001",
+            102,
+            menu_item_payload_second
+        )
+
+    assert exc_info.value.status_code == 400
+    assert exc_info.value.detail == "Cannot add items from different restaurants to the same cart."
+
+# Test that adding multiple items from same restaurant raises no exception
+def test_add_multiple_menu_item_from_same_restaurant(test_carts, mocked_repo,
+                                                     mocked_cart_service, menu_item_payload_second):
+    """Test that add_item_to_cart successfully adds multiple items from the same restaurant"""
+
+    mocked_repo.load_all_carts.return_value = test_carts
+
+    result = mocked_cart_service.add_item_to_cart(
+         "00000000-0000-0000-0000-000000000001",
+         101,
+         menu_item_payload_second
+         )
+    assert len(result.menu_items) == 2
+    assert result.menu_items[1].id == menu_item_payload_second.id
     mocked_repo.save_all_carts.assert_called_once()
