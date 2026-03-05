781e3578ac11b998c68fa7d6d27f3f18b53fbee8
zx01ubc <zx01@student.ubc.ca>
2026-03-04 19:59:38 -0800

    revert previous delete of test_users from args

--- app/test/test_restaurant_integration.py
+++ app/test/test_restaurant_integration.py
@@ -505,14 +505,14 @@ def test_deleting_last_menu_item_fails(test_restaurants,test_users,
 
 #delete_menu_item_from_cart Tests
 
-def test_deleting_menu_item_from_cart_success(test_carts, cart_test_client,
-                                              temp_cart_path):
+def test_deleting_menu_item_from_cart_success(test_carts, test_users,
+                                              cart_test_client, temp_cart_path):
     """Testing a successful deleting of a menu item from a users cart"""
 
     request = "/restaurants/" + str(test_carts[0]["restaurant_id"])
     request = request + "/cart/" + test_carts[0]["id"]
     request = request + "/" + test_carts[0]["cart_items"][0]["item"]["id"]
-    r = cart_test_client.delete(request)
+    r = cart_test_client.delete(request, headers={"user-id": test_users[0]["id"]})
 
     with open(temp_cart_path, "r", encoding="utf-8") as f:
         carts = json.load(f)
