690f66c2f693d7f3129937caef42bcff94c251e5
zxue-01 <zx01@student.ubc.ca>
2026-03-04 17:57:38 -0800

    refactored Cart schema to have CartItem

--- app/schemas/cart.py
+++ app/schemas/cart.py
@@ -5,10 +5,17 @@ from pydantic import BaseModel
 
 from app.schemas.menu import MenuItem
 
+class CartItem(BaseModel):
+    """CartItem entity"""
+    item: MenuItem
+    quantity: int
+
 class Cart(BaseModel):
     """Cart entity"""
     id: str
     user_id: str
     restaurant_id: int
-    menu_items: List[MenuItem] = []
+    cart_items: List[CartItem] = []
+    subtotal: float = 0.00
+    tax: float = 0.00
     total: float = 0.00
