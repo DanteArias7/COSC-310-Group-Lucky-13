e84b3c98e9a204b4bab5f7a84c53c7988b12369d
DanteArias7 <danteari7@gmail.com>
2026-03-06 19:33:10 -0800

    Added order repo class with save order method

---
+++ app/repositories/order_repo.py
@@ -0,0 +1,24 @@
+"""Repository layer for order data."""
+from pathlib import Path
+from typing import Any, Dict
+import pandas
+
+#pylint: disable=too-few-public-methods
+class OrderRepo():
+    """Order repository methods"""
+    def __init__(self, data_path: Path):
+        """Initializes OrderRepo Object with data path to order csv file
+
+        Args:
+            data_path: data path to orders csv file"""
+        self.data_path = data_path
+
+    def save_order(self, order: Dict[str, Any]) -> None:
+        """Saves an order object to the order csv file
+
+        Args:
+            order: An Order object in dict form
+
+        Returns: Nothing"""
+        orderdf = pandas.DataFrame([order])
+        orderdf.to_csv(self.data_path, mode="a", index=False, header=False)
