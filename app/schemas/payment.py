a9640f540b27abadcdc54fea8d6c40ba170bb2b5
Varun Garg <gargvarun2000@gmail.com>
2026-03-11 19:52:27 -0700

    Payment schema added

---
+++ app/schemas/payment.py
@@ -0,0 +1,14 @@
+"""Pydantic schema for payment data."""
+
+from pydantic import BaseModel
+
+class Payment(BaseModel):
+    """Payment request payload."""
+
+    card_number: str
+    cvv: str
+    expiration_date: str
+
+class PaymentResult(BaseModel):
+    """Payment result response."""
+    message: str
