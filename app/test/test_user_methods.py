4323cf2ba3a60b62d6e9d8eedde7ba3d40254baa
DanteArias7 <danteari7@gmail.com>
2026-03-14 14:31:44 -0700

    Added get_user_By_id unit tests

--- app/test/test_user_methods.py
+++ app/test/test_user_methods.py
@@ -1,7 +1,54 @@
 """Unit Tests for users"""
 #pylint: disable=import-error
+from fastapi import HTTPException
+import pytest
 from app.schemas.user import User, UserCreate, UserUpdate
 from app.services.user_services import UserServices
+
+
+def test_get_user_success(mocker):
+    """Scenario: The method should get a user
+    Input: A valid user ID
+    Expected Behviour: The associated user object is returned"""
+
+    mocked_repo = mocker.Mock()
+    mocked_users = [{"id" : "00000000-0000-0000-0000-000000000001", "name" : "Alex", "email" : "alexsmith@gmail.com",
+                "phone_number" : "123-456-7890", "address" : "123 Baron Rd, Kelowna, BC, A1B2C3",
+                "password" : "password",  "role" : "customer"},
+                {"id" : "00000000-0000-0000-0000-000000000002", "name" : "Jeff", "email" : "Jeffsmith@gmail.com",
+                "phone_number" : "555-555-5555", "address" : "321 Ellis Rd, Kelowna, BC, A1B2C3",
+                "password" : "pass",  "role" : "customer"}]
+
+    mocked_repo.load_all_users.return_value =mocked_users
+
+    user_service = UserServices(mocked_repo)
+
+    user = user_service.get_user_by_id("00000000-0000-0000-0000-000000000001")
+
+    assert user.model_dump() == mocked_users[0]
+
+def test_get_nonexistent_user(mocker):
+    """Scenario: The method should raise an error if no user matches
+    Input: An invalid user ID
+    Expected Behviour: A 404 HTTPException error is raised"""
+
+    mocked_repo = mocker.Mock()
+    mocked_users = [{"id" : "00000000-0000-0000-0000-000000000001", "name" : "Alex", "email" : "alexsmith@gmail.com",
+                "phone_number" : "123-456-7890", "address" : "123 Baron Rd, Kelowna, BC, A1B2C3",
+                "password" : "password",  "role" : "customer"},
+                {"id" : "00000000-0000-0000-0000-000000000002", "name" : "Jeff", "email" : "Jeffsmith@gmail.com",
+                "phone_number" : "555-555-5555", "address" : "321 Ellis Rd, Kelowna, BC, A1B2C3",
+                "password" : "pass",  "role" : "customer"}]
+
+    mocked_repo.load_all_users.return_value =mocked_users
+
+    user_service = UserServices(mocked_repo)
+
+    with pytest.raises(HTTPException) as exc_info:
+        user_service.get_user_by_id("00000000-0000-0000-0000-000000000003")
+
+    assert exc_info.value.status_code == 404
+    assert exc_info.value.detail == "User 00000000-0000-0000-0000-000000000003 not found"
 
 def test_create_user(mocker):
     """Unit Test for creating a user method"""
