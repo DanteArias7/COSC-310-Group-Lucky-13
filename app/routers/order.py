06ecc3f1b0b727fbe89a5ab662d6c656a14847c8
zxue-01 <zx01@student.ubc.ca>
2026-03-22 03:03:19 -0700

    added get pending paid orders functionality

--- app/routers/order.py
+++ app/routers/order.py
@@ -145,3 +145,28 @@ def get_all_available_delivery_orders(
     authorization_service = AuthorizationServices(user_repo)
     authorization_service.authorize(user_id, "view_available_deliveries")
     return order_service.get_all_available_delivery_orders()
+
+@order_router.get("/pending", response_model=List[Order], status_code=200)
+def get_all_pending_paid_orders(
+        order_repo: OrderRepo = Depends(create_order_repo),
+        user_repo: UserRepo = Depends(create_user_repo),
+        user_id: str = Header(..., alias="user-id")):
+    """Gets all orders available for restaurant owners to accept/reject.
+
+    Rules:
+    - user must have restaurant_owner role
+
+    Args:
+    order_repo: Order repository
+    user_repo: User repository
+    user_id: current user
+
+    Returns: List of available Order objects
+
+    Raises:
+        403 if user does not have restaurant_owner role
+    """
+    order_service = OrderServices(order_repo)
+    authorization_service = AuthorizationServices(user_repo)
+    authorization_service.authorize(user_id, "view_incoming_orders")
+    return order_service.get_all_pending_paid_orders()
