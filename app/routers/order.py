435a3edfdda1a3a34a4e00cbea9f6b5ac32bf58a
DanteArias7 <danteari7@gmail.com>
2026-03-25 18:05:14 -0700

    renamed get_past_orders_by_restaurant_idendpoint name

--- app/routers/order.py
+++ app/routers/order.py
@@ -15,7 +15,7 @@ from app.schemas.payment import Payment, PaymentResult
 from app.services.restaurant_services import RestaurantServices
 
 ORDER_DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "orders.csv"
-PAST_ORDER_DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "past_orders.csv"
+
 
 order_router = APIRouter(prefix="/orders",
                          tags=["order"])
@@ -28,13 +28,6 @@ def create_order_repo():
     Returns:
             UserRepo object with the order data path attribute"""
     return OrderRepo(ORDER_DATA_PATH)
-
-def create_past_order_repo():
-    """"Initialize repo object with data path to past order data store
-
-    Returns:
-            OrderRepo object with the order data path attribute"""
-    return OrderRepo(PAST_ORDER_DATA_PATH)
 
 def create_restaurant_repo():
     """"Initialize repo object with data path to user data store
@@ -101,15 +94,15 @@ def get_all_orders_for_a_user(order_repo: OrderRepo = Depends(create_order_repo)
     return order_service.get_orders_by_user_id(user_id)
 
 @order_router.get("/{restaurant_id}/past", response_model=List[Order], status_code=200)
-def get_all_past_orders_for_a_restaurant(restaurant_id: int,
+def get_all_orders_for_a_restaurant(restaurant_id: int,
                 order_repo: OrderRepo = Depends(create_order_repo),
                 restaurant_repo: RestaurantRepo = Depends(create_restaurant_repo),
                 user_repo: UserRepo = Depends(create_user_repo),
                 user_id: str = Header(...,alias="user-id")):
-    """Gets all the previous and current orders for a given user.
+    """Gets all the previous and current orders for a given restaurant.
 
     Rules:
-        User must have restaurant_id role
+        User must have restaurant_owner role
 
     Args:
         order_repo: The order repo object to allow order_service to access order data store,
@@ -126,7 +119,7 @@ def get_all_past_orders_for_a_restaurant(restaurant_id: int,
     authorization_service.authorize(user_id, "view_past_orders")
     restaurant_owner_id = restaurant_service.fetch_restaurant(restaurant_id).user_id
     authorization_service.authorize_access(user_id, restaurant_owner_id)
-    return order_service.get_past_orders_by_restaurant_id(restaurant_id)
+    return order_service.get_orders_by_restaurant_id(restaurant_id)
 
 @order_router.put("/{order_id}/accept", response_model=Order, status_code=200)
 def accept_delivery(order_id: str,
