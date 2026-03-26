b0960c442d81b7db023e92237f942dba858ec60a
DanteArias7 <danteari7@gmail.com>
2026-03-25 18:14:22 -0700

    Revert "Added login router to app and changed tag to login" Removing accidental test data added. This reverts commit 1aee7c8e4dd1e90136be5f9d2e22145861e41b5f.

--- app/main.py
+++ app/main.py
@@ -5,13 +5,11 @@ from app.routers.user import user_router
 from app.routers.restaurant import restaurant_router
 from app.routers.order import order_router
 from app.routers.notification_router import notification_router
-from app.routers.login import login_router
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(restaurant_router)
 app.include_router(order_router)
 app.include_router(notification_router)
-app.include_router(login_router)
 
 add_pagination(app)
