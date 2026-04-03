2e4ae6b7cc9fc356c3a04b6ed3be71cf15808b30
DanteArias7 <danteari7@gmail.com>
2026-04-03 13:04:33 -0700

    Added createRating schema for rating creation

--- app/schemas/rating.py
+++ app/schemas/rating.py
@@ -7,3 +7,9 @@ class Rating(BaseModel):
     customer_id: str
     rating: float = Field(ge=0.5, le=5.0, multiple_of=0.5)
     review: str
+
+class CreateRating(BaseModel):
+    """Resturant Rating entity for creating a rating"""
+    customer_id: str
+    rating: float = Field(ge=0.5, le=5.0, multiple_of=0.5)
+    review: str
