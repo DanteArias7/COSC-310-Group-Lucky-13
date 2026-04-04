0c222db997fbd80d145eb2a43737a8f387652f53
DanteArias7 <113080757+DanteArias7@users.noreply.github.com>
2026-04-04 14:53:49 -0700

    Reorganize imports in main.py for clarity

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 """Main application"""
-from fastapi import FastAPI, CORSMiddleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 from app.routers.user import user_router
 from app.routers.restaurant import restaurant_router
