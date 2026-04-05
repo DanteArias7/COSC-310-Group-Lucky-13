3a9d55d55093da44941d6e783de6322b4472083f
Varun Garg <gargvarun2000@gmail.com>
2026-04-04 19:00:34 -0700

    Favorite schema added

---
+++ app/schemas/favorite.py
@@ -0,0 +1,12 @@
+"""Pydantic schema for user favorites."""
+
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Favorite(BaseModel):
+    """Favorite entity."""
+    id: str
+    user_id: str
+    restaurant_id: int
+    menu_item_id: Optional[str] = None
