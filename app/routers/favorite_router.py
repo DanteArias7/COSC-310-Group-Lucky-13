8f41b0f1545e52ae40f1cf5f7d78a569405caa0e
Varun Garg <gargvarun2000@gmail.com>
2026-04-04 19:31:10 -0700

    Repo link added

--- app/routers/favorite_router.py
+++ app/routers/favorite_router.py
@@ -2,9 +2,18 @@
 from pathlib import Path
 from fastapi import APIRouter
 
+from app.repositories.favorite_repo import FavoriteRepo
+
 FAVORITE_DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "favorites.json"
 
 favorite_router = APIRouter(
     prefix="/favorites",
     tags=["favorites"]
 )
+
+def create_favorite_repo():
+    """ Initializes the FavoriteRepo with the path to the favorites data file.
+    Returns:
+        FavoriteRepo: An instance of the FavoriteRepo class.
+    """
+    return FavoriteRepo(FAVORITE_DATA_PATH)
