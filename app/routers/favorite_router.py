d79b59aff5f44eb40cf204e7eb8d23217b063fe4
Varun Garg <gargvarun2000@gmail.com>
2026-04-04 19:24:37 -0700

    Data path added

--- app/routers/favorite_router.py
+++ app/routers/favorite_router.py
@@ -1,6 +1,8 @@
 """API Endpoints for favorite functionality"""
-
+from pathlib import Path
 from fastapi import APIRouter
+
+FAVORITE_DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "favorites.json"
 
 favorite_router = APIRouter(
     prefix="/favorites",
